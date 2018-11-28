1725bf804cb44aa48612f5275f076967a99c9ee2
CesarDSC2 <42946037+CesarDSC2@users.noreply.github.com>
2018-11-27 22:37:20 -0800

    Add files via upload

---
+++ E5-2. Quick Sort/5-2. QuickSortt/Clase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2.QuickSortt
+{
+    class Clase
+    {
+        public void Play()
+        {            
+            double[] ArrayA = new double[] { 11, 2, 3, 14, 18, 45, -3, -1, 6, 9 }; //Creamos el vector con los valores que tendrá cada elemento.
+            Console.WriteLine("1.- Vector sin alterar:");
+            foreach (var item in ArrayA) //Imprimimos los elementos del vector que aún no recibe cambios.
+            {
+                Console.Write(" {0} ;", item);
+            }
+            Proceso A = new Proceso(ArrayA); //Se crea un objeto y mandamos como párametro el vector y ejecuta toda la clase Proceso.
+            //
+            double[] ArrayB = new double[] { 1, 3, 5, 7, 8, 3, 9, .7071, 16.5664, 12, 0, 10 };
+            Console.WriteLine("2.- Vector sin alterar:");
+            foreach (var item in ArrayB)
+            {
+                Console.Write(" {0} ;", item);
+            }
+            Proceso B = new Proceso(ArrayB);
+            //
+            double[] ArrayC = new double[] { 3, 7, 15, 1.33, 155, 100, 15, 1.73, 5, 2, 3, 2 };
+            Console.WriteLine("3.- Vector sin alterar:");
+            foreach (var item in ArrayC)
+            {
+                Console.Write(" {0} ;", item);
+            }
+            Proceso C = new Proceso(ArrayC);
+            //
+            double[] ArrayD = new double[] { 8, 19, 7, 3, 15, 23, 34, 27, 89, 101 };
+            Console.WriteLine("4.- Vector sin alterar:");
+            foreach (var item in ArrayD)
+            {
+                Console.Write(" {0} ;", item);
+            }
+            Proceso D = new Proceso(ArrayC);
+            Console.WriteLine("Salir.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ E5-2. Quick Sort/5-2. QuickSortt/Proceso.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2.QuickSortt
+{
+    class Proceso
+    {
+        double[] Array; //Creamos un vector de la clase Proceso.
+        public Proceso(double[] Array) //Recibimos como párametro el Vector ya inicializado de la Clase.
+        {
+            this.Array = Array; //Asignamos valores del Vector.
+            Play(Array, 0, Array.Length - 1); //Llamamos al método Play que ejecuta el algoritmo necesario para realizar el QuickSort.
+            Desplegar();
+        }
+
+        private void Play(double[] array, int primero, int ultimo) //Recibe como párametros el Vector, el primer índice y el último índice del vector.
+        {
+            int x, y, central; //Creamos variables.
+            double pivote;
+            central = (primero + ultimo) / 2; //Se hace para iniciar el pivote desde el centro del vector.
+            pivote = array[central]; //Se crea un pivote el cual será nuestro puntero.
+            x = primero;
+            y = ultimo;
+            do
+            {
+                while (array[x] < pivote)
+                {
+                    x++; //x aumenta mientras el primer ídice sea menor al pivote.
+                }
+                while (array[y] > pivote)
+                {
+                    y--; //y decrementa mientras el último ídice sea mayor al pivote.
+                }
+                if (x <= y) //Si el primero es menor o igual a el último
+                {
+                    double auxiliar; //Creamos una variable auxiliar
+                    auxiliar = array[x];
+                    array[x] = array[y];
+                    array[y] = auxiliar;
+                    x++;
+                    y--;
+                }
+            } while (x <= y); //Todo eso siempre y cuando el primero sea menor o igual al segundo.
+
+            if (primero < y) 
+            {
+                Play(array, primero, y); //De manera recursiva, si el primero es menor al segundo, se mandan primero los párametros de el primero y el segundo elemento.
+            }
+            if (x < ultimo)
+            {
+                Play(array, x, ultimo); //Y cuando el primero es menor que el último, se mandan despues los párametros de el primero y el último.
+            }
+        }
+        int i;
+        private void Desplegar() //Despliega los elementos ordenados.
+        {
+
+            Console.WriteLine("\n\n- Quick Sort:");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write(" {0} ;", Array[i]);
+            }
+            Console.Write("\n\nPresione enter.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
