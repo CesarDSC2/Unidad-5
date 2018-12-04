4790fd6d072d3f996f4d5427e2c0ae2cc4c0dfc1
CesarDSC2 <42946037+CesarDSC2@users.noreply.github.com>
2018-12-04 14:07:02 -0800

    Add files via upload

---
+++ P3. Examen/P3. Examen/Clase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3.Examen
+{
+    class Clase
+    {
+        public void Play()
+        {
+            Console.Write("¿Cuántos elementos desea introducir?: ");
+            int cant = int.Parse(Console.ReadLine());
+            int[] Array = new int[cant]; //Creamos un Array.
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write("Ingrese el elemento {0}: ", i + 1); //Capturamos valores de cada elemento.
+                Array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+            Console.WriteLine("Vector desordenado: "); //Desplegamos el vector sin alteraciones.
+            foreach (var item in Array)
+            {
+                Console.Write(" {0} ;", item);
+            }
+            Proceso obj = new Proceso();
+            obj.Shell(Array); //Ordenamos el vector.
+            Console.WriteLine("\n-  -   -   -   -   -   -   -   -\nVector ordenado en forma descendente: ");
+            for (int i = Array.Length - 1; i >= 0; i--) //De esta manera se imprime de mayor a menor.
+            {
+                Console.Write(" {0} ;", Array[i]); //Desplegamos el vector alterado.
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P3. Examen/P3. Examen/Proceso.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3.Examen
+{
+    class Proceso
+    {
+        public void Shell(int[] array)
+        {
+            int n = array.Length;
+            int colum = n / 2; //Dividimos el array en 2 partes
+            int temp; //Variable auxiliar
+
+            while (colum > 0)
+            {
+                for (int i = 0; i + colum < n; i++)
+                {
+                    int j = i + colum; //Se crea una variable auxiliar para guardar el valor actual.
+                    temp = array[j]; //Se asigna la variable temporal al arreglo.
+
+                    while (j - colum >= 0 && temp < array[j - colum]) 
+                    {
+                        array[j] = array[j - colum];
+                        j = j - colum; 
+                    }
+
+                    array[j] = temp; //El arreglo adquiere un nuevo valor que guardamos en la variable temporal.
+                }
+
+                colum = colum / 2; //Volvemos a dividir el array.
+            }
+        }
+    }
+}
